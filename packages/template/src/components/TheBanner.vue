0b5bf376f561faca89d478971a094b977a9a5d0a
baoge <huangdabao321@gmail.com>
2022-10-20 15:23:41 +0800

    init: 初始化

---
+++ packages/template/src/components/TheBanner.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+defineProps({
+  title: {
+    type: String,
+    default: '悦车网'
+  },
+})
+</script>
+
+<template>
+  <div class="wrap">
+    <span>{{ msg }}</span>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
